d9bbafbc7e3a61cdaab4f5ee3e1dba33735cf6c6
Christochi <tochukwui2@gmail.com>
2021-12-23 10:16:12 -0500

    ran terraform fmt and validate

--- modules/vault-aws/main.tf
+++ modules/vault-aws/main.tf
@@ -1,14 +1,14 @@
 # create aws secret engine
 resource "vault_aws_secret_backend" "aws" {
 
-    /*
+  /*
      access_key, secret_key, region can be gotten from environment variable
      or shared credential file if already created
     */
-    path = var.mount-path # where aws secret engine will be mounted
-    description = "mounts the aws secret engine"
-    max_lease_ttl_seconds = 600 # the max cap TTL for credentials
-    default_lease_ttl_seconds = 300 # lease duration always < max_lease_ttl_seconds
+  path                      = var.mount-path # where aws secret engine will be mounted
+  description               = "mounts the aws secret engine"
+  max_lease_ttl_seconds     = 600 # the max cap TTL for credentials
+  default_lease_ttl_seconds = 300 # lease duration always < max_lease_ttl_seconds
 
 }
 
@@ -16,10 +16,10 @@ resource "vault_aws_secret_backend" "aws" {
 resource "vault_aws_secret_backend_role" "role" {
 
   # The path the AWS secret backend is mounted at  
-  backend = vault_aws_secret_backend.aws.path 
+  backend = vault_aws_secret_backend.aws.path
 
   # name to identify this role within the backend
-  name    = var.role-name
+  name = var.role-name
 
   credential_type = "iam_user"
 
@@ -45,5 +45,5 @@ data "vault_aws_access_credentials" "creds" {
 
   backend = vault_aws_secret_backend.aws.path
   role    = vault_aws_secret_backend_role.role.name
-  
+
 }
--- modules/vault-aws/variables.tf
+++ modules/vault-aws/variables.tf
@@ -1,11 +1,15 @@
 variable "mount-path" {
-    type = string
-    description = "where aws secret engine will be mounted"
-    default = "tochi-myaws"
+
+  type        = string
+  description = "where aws secret engine will be mounted"
+  default     = "tochi-myaws"
+
 }
 
 variable "role-name" {
-    type = string
-    description = "identifier for the aws role"
-    default = "tochi-myrole" 
+
+  type        = string
+  description = "identifier for the aws role"
+  default     = "tochi-myrole"
+
 }
