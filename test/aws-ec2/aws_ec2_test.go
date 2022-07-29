6592267892e884c6056bc024dff2b385cae37191
Christochi <tochukwui2@gmail.com>
2022-07-29 13:51:54 -0400

    unit test for aws-ec2 module

---
+++ test/aws-ec2/aws_ec2_test.go
@@ -0,0 +1,46 @@
+/*
+	Test Case:
+	- Asserts the correctness of the tag
+	- check if subnet is a public subnet
+
+	Test Type: Unit Test
+*/
+package test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestAWSEC2(t *testing.T) {
+
+	// Construct the terraform options with default retryable errors to handle the most common
+	// retryable errors in terraform testing.
+	// terraform.Options is a struct
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+
+		// Set the path to the Terraform code that will be tested.
+		TerraformDir: "../modules/aws-ec2",
+
+		// variables are passed to terraform using a map
+		Vars: map[string]interface{}{
+			//put something
+
+		},
+	})
+
+	// Clean up resources with "terraform destroy" at the end of the test.
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Run "terraform init" and "terraform apply". Fail the test if there are any errors.
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Run `terraform output` to get the values of output variable
+	subnetID := terraform.Output(t, terraformOptions, "subnet-data")
+
+	// print output value
+	fmt.Printf("The subnet-id is %s\n", subnetID)
+
+}
