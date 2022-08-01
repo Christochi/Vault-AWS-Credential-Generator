d7d4daba4f19be112320d4cd9b263571c964a87b
Christochi <tochukwui2@gmail.com>
2022-08-01 15:56:07 -0400

    terraform.Options

--- test/vault-aws/vault_aws_test.go
+++ test/vault-aws/vault_aws_test.go
@@ -15,4 +15,12 @@ import (
 
 func TestVaultAWS(t *testing.T) {
 
+	// Construct the terraform options with default retryable errors to handle the most common
+	// retryable errors in terraform testing.
+	// terraform.Options is a struct
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+
+		// Set the path to the Terraform code that will be tested.
+		TerraformDir: "../../modules/vault-aws",
+	})
 }
